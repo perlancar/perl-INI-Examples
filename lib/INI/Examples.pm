5057665758b147b416a98036b246bf2a7c961804
perlancar (on pc-office) <perlancar@gmail.com>
2019-04-23 13:25:15 +0700

    Initial commit

---
+++ lib/INI/Examples.pm
@@ -0,0 +1,20 @@
+package INI::Examples;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Example INI configuration files
+
+=head1 DESCRIPTION
+
+This distribution contains various examples of INI configuration files in its
+per-dist share dir.
+
+
+=head1 SEE ALSO
+
+Some modules to parse INI files: L<Config::INI::Reader>, L<Config::IniFiles>,
+L<Config::IOD::INI::Reader>.
+
+Related examples: L<IOD::Examples>
